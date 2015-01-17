986c9e9b9627192adb8bad6d1900e95d22b7aa68
Sean McIntyre <seanmcintyre@gmail.com>
2015-01-17 11:52:24 -0500

    early front-end experiments

---
+++ _build/js/wordlist.js
@@ -0,0 +1,56 @@
+// TODO
+// No need to parse transcript into array in production.
+// Do it once in development, and paste the array in statically.
+Array.prototype.unique = function() {
+    return this.reduce(function(accum, current) {
+        if (accum.indexOf(current) < 0) {
+            accum.push(current);
+        }
+        return accum;
+    }, []);
+};
+var words = transcript.replace(/\s+/g, '-').replace(/[^a-zA-Z-]/g, '').toLowerCase().split('-').unique();
+
+// Textarea autocompletion
+$('textarea').textcomplete([
+    {
+        words: words,
+        match: /\b(\w{1,})$/,
+        search: function (term, callback) {
+            callback($.map(this.words, function (word) {
+                return word.indexOf(term) === 0 ? word : null;
+            }));
+        },
+        index: 1,
+        replace: function (word) {
+            return word + ' ';
+        }
+    }
+]);
+
+
+// TODO
+// Disable linebreaks, and disable spacebar unless a matching word or
+// phrase exists.  Enter and spacebar will autocomplete selected word.
+$('textarea').keypress(function(event) {
+    if (event.keyCode == 13) {
+        event.preventDefault();
+    } else if (event.keyCode == 32) {
+        event.preventDefault();
+    }
+});
+
+// TODO
+// Make the textarea a little more magical
+var scrollTop     = $(window).scrollTop(),
+    elementOffset = $('textarea').offset().top,
+    distance      = (elementOffset - scrollTop);
+
+$('textarea').keyup(function(){
+	var charRow = Math.ceil($('textarea').val().length/36),
+			rows = (charRow > 1) ? charRow : 1,
+			elementOffset = $('textarea').offset().top,
+			distance      = (elementOffset - scrollTop)
+  $('.charCount').text("Characters left: " + (200 - $('textarea').val().length));
+  $('.cursor').css('top',$('textarea').textareaHelper('caretPos').top + distance);
+});
---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+var 	gulp = require('gulp'),
+			sass = require('gulp-sass'),
+			jshint = require('gulp-jshint'),
+			stylish = require('jshint-stylish'),
+			concat = require('gulp-concat'),
+			uglify = require('gulp-uglify'),
+			rename = require('gulp-rename'),
+			//connect = require('gulp-connect'),
+			browserSync = require('browser-sync'),
+			autoprefixer = require('gulp-autoprefixer'),
+			minifyCSS = require('gulp-minify-css');
+
+// Static server
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: "app"
+		},
+		port: 8080
+	});
+});
+
+gulp.task('js', function() {
+	return gulp.src('_build/js/*.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter(stylish))
+		.pipe(concat('site.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('app/js/'))
+		//.pipe(connect.reload());
+		.pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('scss', function() {
+	return gulp.src('_build/scss/style.scss')
+		.pipe(sass())
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions', 'ie >= 8'],
+			cascade: false
+		}))
+		.pipe(minifyCSS())
+		.pipe(gulp.dest('app/css/'))
+		.pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('html', function() {
+	gulp.src('_build/html/**')
+		.pipe(gulp.dest('app'))
+		.pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('_build/js/**', ['js']);
+	gulp.watch('_build/scss/**', ['scss']);
+	gulp.watch('_build/html/**', ['html']);
+});
+
+gulp.task('default', []);
+
+gulp.task('serve', ['browser-sync'], function() {
+	gulp.watch('_build/js/**', ['js']);
+	gulp.watch('_build/scss/**', ['scss']);
+	gulp.watch('_build/html/**', ['html']);
+});
+
+gulp.task('build', ['scss', 'js', 'html', 'watch']);
