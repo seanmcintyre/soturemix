4d329affc02eb97cd5647e2ae011f1f277acf6c3
Sean McIntyre <seanmcintyre@gmail.com>
2015-01-18 14:31:16 -0500

    more progress on custom phrase selection

--- _build/js/wordlist.js
+++ _build/js/wordlist.js
@@ -12,58 +12,98 @@ Array.prototype.unique = function() {
 var words = transcript.replace(/\s+/g, '-').replace(/[^a-zA-Z-]/g, '').toLowerCase().split('-').unique();
 
 var phrases = ['this is', 'a funny', 'bone tired', 'day in america', 'a united people', 'iran', 'pizza', 'with', 'for', 'and', 'my', 'republican friends', 'tomorrow'];
-phrases = phrases.reduce(function(o, v, i) {
-  o[v] = i+1;
-  return o;
-}, {});
+// phrases = phrases.reduce(function(o, v, i) {
+//   o[v] = i+1;
+//   return o;
+// }, {});
+
+//var phrases = words;
 
 // Textarea autocompletion
-$('textarea').textcomplete([
-    {
-        words: words,
-        match: /\b(\w{1,})$/,
-        search: function (term, callback) {
-            callback($.map(this.words, function (word) {
-                return word.indexOf(term) === 0 ? word : null;
-            }));
-        },
-        index: 1,
-        replace: function (word) {
-            return word + ' ';
-        }
-    }
-]);
+// $('textarea').textcomplete([
+//     {
+//         words: words,
+//         match: /\b(\w{1,})$/,
+//         search: function (term, callback) {
+//             callback($.map(this.words, function (word) {
+//                 return word.indexOf(term) === 0 ? word : null;
+//             }));
+//         },
+//         index: 1,
+//         replace: function (word) {
+//             return word + ' ';
+//         }
+//     }
+// ]);
+
+
 // TAG IT (alternative)
 // $('textarea').tagit({
 //     availableTags: phrases
 // });
 
+
+// $('.add-phrase').onkeydown = function(event) {
+//     console.log('a');
+//     if( event.keyCode == 8 ) {
+//         console.log('BACKSPACE');
+//         return false;
+//     }
+// };
+
 var matches = [];
-$('.add-phrase').keypress(function(event) {
+$('.add-phrase').keyup(function(event) {
     var text = $(this).val();
-    var textAndKey = text + String.fromCharCode(event.keyCode);
     matches = [];
 
     // Loop through phrases to display matches
-    var subStr = new RegExp(textAndKey);
-    for (phrase in phrases) {
-        if (subStr.test(phrase) & matches.indexOf(textAndKey) === -1) {
+    var subStr = new RegExp(text);
+
+    //
+    // If we use an object of phrases instead of array:
+    //
+    // for (phrase in phrases) {
+    //     console.log(phrase);
+    //     if (subStr.test(phrase) & matches.indexOf(textAndKey) === -1) {
+    //         matches.push(phrase);
+    //         console.log(matches);
+    //     }
+    // }
+
+    // If we use an array:
+    //
+    var phrase;
+    var matchList = '';
+    for (var i = 0; i < phrases.length; i++) {
+        phrase = phrases[i];
+        if (subStr.test(phrase) & matches.indexOf(text) === -1) {
             matches.push(phrase);
-            console.log(matches);
+            if (phrase.length > 0) {
+            matchList = matchList + '<li>'+phrase+'</li>';
+            //console.log(matches);
+            }
         }
     }
-    $('.matches').text(matches);
+    $('.matches').html(matchList);
 
     // If user hits enter key
     if (event.keyCode == 13) {
-        if (phrases[text] > -1) {
+        if (phrases.indexOf(text) > -1) {
             console.log('correct phrase! : ' + text);
-            $('.test-two').prepend('<li>'+text+'</li>');
+            $('.input').before('<li>'+text+'</li>');
             matches = [];
+            matchList = '';
+            $(this).val('')
         } else {
             console.log('not a phrase! : ' + text);
         }
     }
+
+    if( event.keyCode == 8 ) {
+        console.log('BACKSPACE');
+        return false;
+    }
+
 });
 
 
@@ -71,17 +111,17 @@ $('.add-phrase').keypress(function(event) {
 // Disable linebreaks, and disable spacebar unless a matching word or
 // phrase exists.  Enter and spacebar will autocomplete selected word.
 // Allow spaces after periods.
-var lastKeys = [0,0];
-$('textarea').keypress(function(event) {
-    lastKeys.push(event.keyCode);
-    lastKeys.shift();
-    console.log(lastKeys);
-    if (event.keyCode == 13) {
-        event.preventDefault();
-    } else if (event.keyCode == 32 && lastKeys[0] != 46) {
-        event.preventDefault();
-    }
-});
+// var lastKeys = [0,0];
+// $('textarea').keypress(function(event) {
+//     lastKeys.push(event.keyCode);
+//     lastKeys.shift();
+//     console.log(lastKeys);
+//     if (event.keyCode == 13) {
+//         event.preventDefault();
+//     } else if (event.keyCode == 32 && lastKeys[0] != 46) {
+//         event.preventDefault();
+//     }
+// });
 
 //
 // completeOnSpace must be set to true in textcomplete.js
@@ -89,15 +129,15 @@ $('textarea').keypress(function(event) {
 
 // TODO
 // Make the textarea a little more magical
-var scrollTop     = $(window).scrollTop(),
-    elementOffset = $('textarea').offset().top,
-    distance      = (elementOffset - scrollTop);
+// var scrollTop     = $(window).scrollTop(),
+//     elementOffset = $('textarea').offset().top,
+//     distance      = (elementOffset - scrollTop);
 
-$('textarea').keyup(function(){
-	var charRow = Math.ceil($('textarea').val().length/36),
-			rows = (charRow > 1) ? charRow : 1,
-			elementOffset = $('textarea').offset().top,
-			distance      = (elementOffset - scrollTop)
-  $('.charCount').text("Characters left: " + (200 - $('textarea').val().length));
-  $('.cursor').css('top',$('textarea').textareaHelper('caretPos').top + distance);
-});
+// $('textarea').keyup(function(){
+// 	var charRow = Math.ceil($('textarea').val().length/36),
+// 			rows = (charRow > 1) ? charRow : 1,
+// 			elementOffset = $('textarea').offset().top,
+// 			distance      = (elementOffset - scrollTop)
+//   $('.charCount').text("Characters left: " + (200 - $('textarea').val().length));
+//   $('.cursor').css('top',$('textarea').textareaHelper('caretPos').top + distance);
+// });
