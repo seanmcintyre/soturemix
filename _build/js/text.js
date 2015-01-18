752de71cfb931b8ebdcfff6933f9b36c2cc2c5bf
Sean McIntyre <seanmcintyre@gmail.com>
2015-01-18 18:40:03 -0500

    cleanup

--- _build/js/text.js
+++ _build/js/text.js
@@ -4,44 +4,16 @@ var matches = require('./matches');
 var DataManager = require('./lib/DataManager');
 var VideoPlayer  = require('./lib/HTMLVideoPlayer');
 
-window.foo = 'bar';
-
-// TODO
-// No need to parse transcript into array in production.
-// Do it once in development, and paste the array in statically.
-// Array.prototype.unique = function() {
-//     return this.reduce(function(accum, current) {
-//         if (accum.indexOf(current) < 0) {
-//             accum.push(current);
-//         }
-//         return accum;
-//     }, []);
-// };
-
 var lastKey;
-
 var phrases = []; // will be filled in by network call
-// var matches = [];
-// var match = $('.match');
-// var selectedMatch;
-// var selectedMatchText;
-// var newMatch;
-
 var text;
 var subStr;
 
 DataManager.getAvailablePhrases(function (err, availablePhrases) {
     phrases = availablePhrases;
-    // phrases = phrases.reduce(function(o, v, i) {
-    //   o[v] = i+1;
-    //   return o;
-    // }, {});
-    // var words = transcript.replace(/\s+/g, '-').replace(/[^a-zA-Z-]/g, '').toLowerCase().split('-').unique();
-    // var phrases = words;
 });
 
 $('.add-phrase').keyup(function(event) {
-
     text = $(this).val();
     matches.matchList = [];
 
@@ -88,5 +60,3 @@ $('#vamanos').on('click', function () {
     videoPlayer.load(theSpeech.text);
     videoPlayer.playWhenReady();
 });
-
-window.foo = 'bar';
