1c3b928343332823b6b7802e3c6f2205820edb9c
Sean McIntyre <seanmcintyre@gmail.com>
2015-01-18 15:20:18 -0500

    getting better

--- _build/js/wordlist.js
+++ _build/js/wordlist.js
@@ -19,39 +19,10 @@ var phrases = ['this is', 'a funny', 'bone tired', 'day in america', 'a united p
 
 //var phrases = words;
 
-// Textarea autocompletion
-// $('textarea').textcomplete([
-//     {
-//         words: words,
-//         match: /\b(\w{1,})$/,
-//         search: function (term, callback) {
-//             callback($.map(this.words, function (word) {
-//                 return word.indexOf(term) === 0 ? word : null;
-//             }));
-//         },
-//         index: 1,
-//         replace: function (word) {
-//             return word + ' ';
-//         }
-//     }
-// ]);
-
-
-// TAG IT (alternative)
-// $('textarea').tagit({
-//     availableTags: phrases
-// });
-
-
-// $('.add-phrase').onkeydown = function(event) {
-//     console.log('a');
-//     if( event.keyCode == 8 ) {
-//         console.log('BACKSPACE');
-//         return false;
-//     }
-// };
-
 var matches = [];
+var lastKey;
+var remixedSOTU = [];
+
 $('.add-phrase').keyup(function(event) {
     var text = $(this).val();
     matches = [];
@@ -59,85 +30,44 @@ $('.add-phrase').keyup(function(event) {
     // Loop through phrases to display matches
     var subStr = new RegExp(text);
 
-    //
-    // If we use an object of phrases instead of array:
-    //
-    // for (phrase in phrases) {
-    //     console.log(phrase);
-    //     if (subStr.test(phrase) & matches.indexOf(textAndKey) === -1) {
-    //         matches.push(phrase);
-    //         console.log(matches);
-    //     }
-    // }
-
-    // If we use an array:
-    //
-    var phrase;
-    var matchList = '';
-    for (var i = 0; i < phrases.length; i++) {
-        phrase = phrases[i];
-        if (subStr.test(phrase) & matches.indexOf(text) === -1) {
-            matches.push(phrase);
-            if (phrase.length > 0) {
-            matchList = matchList + '<li>'+phrase+'</li>';
-            //console.log(matches);
+    if (text != '') {
+        var phrase;
+        var matchList = '';
+        for (var i = 0; i < phrases.length; i++) {
+            phrase = phrases[i];
+            if (subStr.test(phrase) & matches.indexOf(text) === -1) {
+                matches.push(phrase);
+                if (phrase.length > 0) {
+                matchList = matchList + '<li>'+phrase+'</li>';
+                }
             }
         }
+        $('.matches').html(matchList);
+    } else {
+        $('.matches').html('');
     }
-    $('.matches').html(matchList);
 
     // If user hits enter key
     if (event.keyCode == 13) {
         if (phrases.indexOf(text) > -1) {
-            console.log('correct phrase! : ' + text);
             $('.input').before('<li>'+text+'</li>');
+            remixedSOTU.push(text);
             matches = [];
             matchList = '';
             $(this).val('')
-        } else {
-            console.log('not a phrase! : ' + text);
+            console.log('The speech: ' , remixedSOTU);
         }
     }
 
     if( event.keyCode == 8 ) {
         console.log('BACKSPACE');
-        return false;
+        if (text === '' && $('.input').prev('li')[0] && (lastKey == 8 || lastKey == 13)) {
+            $('.input').prev('li')[0].remove();
+            remixedSOTU = remixedSOTU.splice(-1,1);
+
+        }
     }
 
+    lastKey = event.keyCode;
+
 });
-
-
-// TODO
-// Disable linebreaks, and disable spacebar unless a matching word or
-// phrase exists.  Enter and spacebar will autocomplete selected word.
-// Allow spaces after periods.
-// var lastKeys = [0,0];
-// $('textarea').keypress(function(event) {
-//     lastKeys.push(event.keyCode);
-//     lastKeys.shift();
-//     console.log(lastKeys);
-//     if (event.keyCode == 13) {
-//         event.preventDefault();
-//     } else if (event.keyCode == 32 && lastKeys[0] != 46) {
-//         event.preventDefault();
-//     }
-// });
-
-//
-// completeOnSpace must be set to true in textcomplete.js
-//
-
-// TODO
-// Make the textarea a little more magical
-// var scrollTop     = $(window).scrollTop(),
-//     elementOffset = $('textarea').offset().top,
-//     distance      = (elementOffset - scrollTop);
-
-// $('textarea').keyup(function(){
-// 	var charRow = Math.ceil($('textarea').val().length/36),
-// 			rows = (charRow > 1) ? charRow : 1,
-// 			elementOffset = $('textarea').offset().top,
-// 			distance      = (elementOffset - scrollTop)
-//   $('.charCount').text("Characters left: " + (200 - $('textarea').val().length));
-//   $('.cursor').css('top',$('textarea').textareaHelper('caretPos').top + distance);
-// });
