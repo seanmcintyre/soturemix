6e49e430c60bfcc0a65742f7a37e34a72b2f7062
Max Mamis <maxmamis@gmail.com>
2015-01-24 13:47:44 -0500

    detect mobile safari

--- _build/js/lib/video/VideoPlayer.js
+++ _build/js/lib/video/VideoPlayer.js
@@ -2,7 +2,7 @@ var HTMLVideoPlayer = require('./HTMLVideoPlayer');
 var JPEGVideoPlayer = require('./JPEGVideoPlayer');
 var path = require('path');
 
-var isMobile = false;
+var isMobile = /iphone/i.test(navigator.userAgent.toLowerCase());
 
 function VideoPlayer ($container, clipRoot) {
 	if (isMobile) {
