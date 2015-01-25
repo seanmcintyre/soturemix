fc0a3c8c126baaf19f484352393240f97971dc03
Max Mamis <maxmamis@gmail.com>
2015-01-25 00:06:29 -0500

    changed prod hostname to temp subdomain

--- config/config.deploy.js
+++ config/config.deploy.js
@@ -4,8 +4,8 @@ module.exports = {
 	"port": 80,
 	"appName": "sotuRemixDev",
 	"clipsDirectory": "../app/video/desktop",
-	"rootURL": "",
-	"api_root": "",
+	"rootURL": "http://sotu.maxmamis.com/videos/",
+	"api_root": "/",
 	"templateFile": "_build/html/index.moustache",
 	"remote_server": "104.131.184.172"
 }
